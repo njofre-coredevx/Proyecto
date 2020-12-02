dc98c7209e81ff9c1c2909cdc75e32ba243478aa
njofre-coredevx <74695672+njofre-coredevx@users.noreply.github.com>
2020-12-02 02:38:44 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,90 @@
+###################################################################################
+###                              LIBRERÍAS                                      ###
+###################################################################################
+
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
-imagenes = {}
-for i in range(6, 56):
-    if i <10:
-        p = f"0{i}"
-    else:
-        p = i
-    imagen = cv2.imread(f"images/ISIC_00243{p}.jpg")
-    segmentation = cv2.imread(f"images/ISIC_00243{p}_segmentation.png")
-    imagenes[f"imagen {i}"] = [imagen, segmentation]
+
+###################################################################################
+###                           CARGANDO IMAGENES                                 ###
+###################################################################################
+
+def cargar_imagenes():
+    imagenes = {}
+    for i in range(6, 56):
+        if i <10:
+            p = f"0{i}"
+        else:
+            p = i
+        imagen = cv2.imread(f"images/ISIC_00243{p}.jpg")
+        segmentation = cv2.imread(f"images/ISIC_00243{p}_segmentation.png")
+        imagenes[f"imagen {i}"] = [imagen, segmentation]
+    return imagenes
+
+''' 
+def cargar_test(l):
+    l:lista
+'''
+###################################################################################
+###                          CALCULO DE ERROR                                   ###
+###################################################################################
+def calculando_error(ideal, real):
+    N, M = ideal.shape
+    TP, TN, FP, FN = 0, 0, 0, 0 
+    for i in range(N):
+        for j in range(M):
+            if ideal[i,j] == real[i,j] == 1:
+                TP += 1
+            elif ideal[i,j] == real[i,j] == 0:
+                TN += 1
+            elif ideal[i,j] != real[i,j] == 1:
+                FP += 1
+            elif ideal[i, j] != real[i,j] == 0:
+                FN += 1
+    TPR = TP / (TP+FN)
+    FPR = FP/(FP+TN)
+    return TPR, FPR
+              
+                
+  
+######### Función Segmentar ###########
+def segmentar(I, l):
+    ## Permite obtener la segmentación de una imagen en escala de grises para un rango de valores entre 0 y 255
+    ## I: imagen en escala de grises; l = intervalo de segmentación de tipo [v_min, v_max]
+    N, M = I.shape
+    J = np.zeros((N,M))
+    for i in range(N):
+        for j in range(M):
+            if I[i,j] >= l[0] and I[i,j]<=l[-1]:
+                J[i,j] = 1
+    return J 
+      
+######### Escala de Grises ###########
+def escalas_de_grises(dic_imagenes):
+    escala_grises = []
+    for llave in dic_imagenes:
+        gray = cv2.cvtColor(dic_imagenes[llave][0], cv2.COLOR_BGR2GRAY)
+        escala_grises.append(gray)
+    return escala_grises
+
+######### Filtro Mediana ###########
+def filtro_mediana(imagenes):
+    median_image = []
+    for imagen in imagenes:
+        n = int(imagen.shape[0] * imagen.shape[1] / 10000)
+        median_image.append(cv2.medianBlur(imagen, n))
+    return median_image
+
+'''    
 def filtro_gauss():
-    
+    pass  
+'''
+imagenes = cargar_imagenes()
 
 plt.figure()
 plt.subplot(1, 2, 1)
-plt.imshow(imagenes["imagen 19"][0])
+plt.imshow(imagenes["imagen 6"][0])
 plt.title("Imagen Original")
 plt.subplot(1, 2, 2)
 plt.imshow(imagenes["imagen 19"][1])
